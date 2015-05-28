6cda2dac120fb3384147c1bd5a7a33f5e3912bc0
budostylz <lewis.shaun1@gmail.com>
2015-05-28 08:20:25 +0200

    'commit'

---
+++ README.md
@@ -0,0 +1,1 @@
+## Repository for Custom and CSS Libraries
---
+++ css/test_css.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+	     <title>Test Responsive Layouts</title>
+	     <link href="col.css" rel ="stylesheet" type="text/css">
+
+	     <meta charset ='utf-8'>
+    </head>
+
+    <body>
+      
+        <div class= 'col' style='background-color:blue' >Blue</div>
+
+    </body>
+</html>
