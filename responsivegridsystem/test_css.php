9a3aec20d735c37b6a61a5bf1e51b86ab74569dd
budostylz <lewis.shaun1@gmail.com>
2015-05-28 10:53:52 +0200

    'commit'

---
+++ responsivegridsystem/test_css.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+	     <title>Test Responsive Layouts</title>
+	     <link href="3cols.css" rel ="stylesheet" type="text/css">
+
+	     <meta charset ='utf-8'>
+    </head>
+
+    <body>
+      
+      <div id='wrapper' style='margin-left:120px'>
+        <div class= 'span_1_of_3' style='background-color:blue; float:left' >Blue</div>
+        <div class= 'span_2_of_3' style='background-color:green; float:left' >Green</div>
+        <div class= 'span_1_of_3' style='background-color:yellow; float:left' >Yellow</div>
+      </div>
+    </div>
+    </body>
+</html>
